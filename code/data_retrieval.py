25c8cdb3481b5c9f590b1976e0201a1af6faaa05
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-10-25 13:59:33 +0100

    added data retrieval code using astroquery

---
+++ code/data_retrieval.py
@@ -0,0 +1,13 @@
+from astroquery.alma import Alma
+
+a = Alma()
+a.login('keflavich')
+
+# Find my project
+result = a.query_object('W51', payload=dict(pi_name='ginsburg'), public=False)
+
+uid_url_table = a.stage_data(result['Member ous id'])
+
+filelist = a.download_and_extract_files(uid_url_table['URL'],
+                                        include_asdm=True, regex='.*',
+                                        delete=True, verbose=True)
