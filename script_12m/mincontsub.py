1d728afaa471ffe3f549bdedc07e44f9a3ea7280
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-11-04 16:56:00 +0100

    mincontsub revised to subtract off the continuum-subtracted spectral cube.  It still doesn't work...

--- script_12m/mincontsub.py
+++ script_12m/mincontsub.py
@@ -17,33 +17,30 @@ for suffix in ('image','model','flux','psf','residual'):
 import spectral_cube
 cube = spectral_cube.SpectralCube.read('test_frequency.image.fits')
 cont = cube.min(axis=0)
-cont.meta['beam'] = cube.beam
-hdu = cont.hdu
-hdu.header['CRPIX3'] = 1
-hdu.header['CRVAL3'] = cube.header['CRVAL3']
-hdu.header['CDELT3'] = cube.header['CDELT3']
-hdu.header['CTYPE3'] = 'FREQ'
+scube = cube - cont
+hdu = cube.hdu
+hdu.data = hdu.data.reshape((1,) + hdu.data.shape)
 hdu.header['CRPIX4'] = 1
 hdu.header['CRVAL4'] = 1
 hdu.header['CDELT4'] = 1
 hdu.header['CTYPE4'] = 'STOKES'
-hdu.writeto('test_continuum_min.fits', clobber=True)
-importfits('test_continuum_min.fits', 'test_continuum_min.image', overwrite=True)
+hdu.writeto('test_frequency_minsub.fits', clobber=True)
+importfits('test_frequency_minsub.fits', 'test_frequency_minsub.image', overwrite=True)
 
-os.system('rm -rf w51_test_small_imcont.ms')
-split('w51_test_small.ms', 'w51_test_small_imcont.ms', datacolumn='data')
+os.system('rm -rf w51_test_small_linecubesub.ms')
+split('w51_test_small.ms', 'w51_test_small_linecubesub.ms', datacolumn='data')
+ft(vis='w51_test_small_linecubesub.ms', model='test_frequency_minsub.image', usescratch=True, nterms=1)
 raise ValueError("ft does not work.")
-ft(vis='w51_test_small_imcont.ms', model='test_continuum_min.image', usescratch=True, nterms=1)
-uvsub('w51_test_small_imcont.ms')
+uvsub('w51_test_small_linecubesub.ms')
 
-os.system('rm -rf test_frequency_mincontsub.*')
-clean(vis='w51_test_small_imcont.ms', imagename="test_frequency_mincontsub",
-      field="", spw='', mode='channel', outframe='LSRK',
+os.system('rm -rf test_mfs_linecubesub.*')
+clean(vis='w51_test_small_linecubesub.ms', imagename="test_mfs_linecubesub",
+      field="", spw='', mode='mfs', outframe='LSRK',
       interpolation='linear', imagermode='mosaic', interactive=False,
       niter=10000, threshold='50.0mJy', imsize=[512,512], cell='0.052arcsec',
       weighting='briggs', phasecenter='J2000 19h23m43.905 +14d30m28.08',
       pbcor=False, usescratch=True, robust=1.0)
-exportfits('test_frequency_mincontsub.image', 'test_frequency_mincontsub.image.fits', dropdeg=True, overwrite=True)
-exportfits('test_frequency_mincontsub.model', 'test_frequency_mincontsub.model.fits', dropdeg=True, overwrite=True)
+exportfits('test_mfs_linecubesub.image', 'test_mfs_linecubesub.image.fits', dropdeg=True, overwrite=True)
+exportfits('test_mfs_linecubesub.model', 'test_mfs_linecubesub.model.fits', dropdeg=True, overwrite=True)
 for suffix in ('image','model','flux','psf','residual'):
-    os.system('rm -rf test_frequency_mincontsub.{0}'.format(suffix))
+    os.system('rm -rf test_mfs_linecubesub.{0}'.format(suffix))
