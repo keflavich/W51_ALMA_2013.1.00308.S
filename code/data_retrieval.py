0e5e8710c8a2b989defd201055afa1f08cb2d99a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-11-10 18:55:46 +0100

    comments in data retrieval

--- code/data_retrieval.py
+++ code/data_retrieval.py
@@ -5,7 +5,10 @@ a.login('keflavich')
 
 # Find my project
 result = a.query_object('W51', payload=dict(pi_name='ginsburg'), public=False)
+_7m = result['Spatial resolution'] > 3
+_12m = result['Spatial Resolution'] < 3
 
+# unfortunately, ALMA doesn't support querying for MOUSes
 uid_url_table = a.stage_data(result['Member ous id'])
 
 filelist = a.download_and_extract_files(uid_url_table['URL'],
