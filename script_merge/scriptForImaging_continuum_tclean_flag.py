7b8e33189871902b981f20ea3d9e49f56a4c6530
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-11-30 17:20:47 +0100

    add imaging script with flagging + tclean

---
+++ script_merge/scriptForImaging_continuum_tclean_flag.py
@@ -0,0 +1,155 @@
+def clean(vis, imagename, **kwargs):
+    tclean(vis = vis,
+           imagename = imagename,
+           **kwargs)
+ #         field = '',
+ #         spw = '', # there should be only one
+ #         specmode = 'cube',
+ #         width = width,
+ #         start = startfreq,
+ #         nchan = nchans_per_cube,
+ #         veltype = 'radio',
+ #         outframe = 'LSRK',
+ #          gridder='mosaic',
+ #          deconvolver='clark',
+ #         interactive = F,
+ #         niter = 25000,
+ #         imsize = imsize,
+ #         cell = cell,
+ #         weighting = weighting,
+ #         phasecenter = phasecenter,
+ #         robust = robust,
+ #         threshold = threshold,
+ #         savemodel='none',
+ #         overwrite=True)
+
+"""
+Attempt to image the continuum with flagging
+"""
+
+mergevis = 'continuum_7m12m.ms'
+if not os.path.exists(mergevis):
+    raise ValueError("Make sure to run scriptForImaging_continuum_flag.py first")
+
+extensions = ['.flux', '.image', '.mask', '.model', '.pbcor', '.psf',
+              '.residual', '.flux.pbcoverage', '.sumwt', '.weight', '.pb',
+              '.pbcoverage']
+
+contimagename = 'w51_spw3_continuum_7m12m_flagged_tclean'
+
+for ext in extensions:
+    rmtables(contimagename+ext)
+
+clean(vis=mergevis,
+      imagename=contimagename,
+      field='w51',
+      phasecenter='',
+      specmode='mfs',
+      deconvolver='clark',
+      imsize = [960,960],
+      cell= '0.15arcsec',
+      weighting = 'natural',
+      robust = 2.0,
+      niter = 10000,
+      threshold = '1.0mJy',
+      interactive = False,
+      gridder = 'mosaic',
+      savemodel='none',
+      )
+exportfits(contimagename+".image", contimagename+".image.fits", dropdeg=True, overwrite=True)
+
+
+contimagename = 'w51_spw3_continuum_7m12m_flagged_r0_tclean'
+
+for ext in extensions:
+    rmtables(contimagename+ext)
+
+clean(vis=mergevis,
+      imagename=contimagename,
+      field='w51',
+      phasecenter='',
+      specmode='mfs',
+      deconvolver='clark',
+      imsize = [2560,2560],
+      cell= '0.052arcsec',
+      weighting = 'briggs',
+      robust = 0.0,
+      niter = 10000,
+      threshold = '1.0mJy',
+      interactive = False,
+      gridder = 'mosaic',
+      savemodel='none',
+      )
+exportfits(contimagename+".image", contimagename+".image.fits", dropdeg=True, overwrite=True)
+
+contimagename = 'w51_spw3_continuum_7m12m_flagged_r0_dirty_tclean'
+
+for ext in extensions:
+    rmtables(contimagename+ext)
+
+clean(vis=mergevis,
+      imagename=contimagename,
+      field='w51',
+      phasecenter='',
+      specmode='mfs',
+      deconvolver='clark',
+      imsize = [2560,2560],
+      cell= '0.052arcsec',
+      weighting = 'briggs',
+      robust = 0.0,
+      niter = 0,
+      threshold = '1.0mJy',
+      interactive = False,
+      gridder = 'mosaic',
+      savemodel='none',
+      )
+exportfits(contimagename+".image", contimagename+".image.fits", dropdeg=True, overwrite=True)
+
+contimagename = 'w51_spw3_continuum_7m12m_flagged_r0_multiscale_tclean'
+
+for ext in extensions:
+    rmtables(contimagename+ext)
+
+clean(vis=mergevis,
+      imagename=contimagename,
+      field='w51',
+      scales=[0,3,6,9,12,15,18],
+      phasecenter='',
+      specmode='mfs',
+      deconvolver='multiscale',
+      imsize = [2560,2560],
+      cell= '0.052arcsec',
+      weighting = 'briggs',
+      robust = 0.0,
+      niter = 10000,
+      threshold = '10.0mJy',
+      interactive = False,
+      gridder = 'mosaic',
+      savemodel='none',
+      )
+exportfits(contimagename+".image", contimagename+".image.fits", dropdeg=True, overwrite=True)
+
+contimagename = 'w51_spw3_continuum_7m12m_flagged_r0_MEM_tclean'
+
+for ext in extensions:
+    rmtables(contimagename+ext)
+
+clean(vis=mergevis,
+      imagename=contimagename,
+      field='w51',
+      scales=[0,3,6,9,12,15,18],
+      phasecenter='',
+      specmode='mfs',
+      deconvolver='mem',
+      imsize = [2560,2560],
+      cell= '0.052arcsec',
+      weighting = 'briggs',
+      robust = 0.0,
+      niter = 10000,
+      threshold = '10.0mJy',
+      interactive = False,
+      gridder = 'mosaic',
+      savemodel='none',
+      )
+exportfits(contimagename+".image", contimagename+".image.fits", dropdeg=True, overwrite=True)
+
