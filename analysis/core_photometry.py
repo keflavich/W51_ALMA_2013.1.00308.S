da7914f1dbdcf79d050e0375bff2d5d6dc2a1df8
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-12-31 15:33:45 +0100

    added core photometry table writing

--- analysis/core_photometry.py
+++ analysis/core_photometry.py
@@ -1,13 +1,57 @@
 import numpy as np
-from spectral_cube import SpectralCube
 from astropy import units as u
+from astropy import log
 import paths
 import pyregion
 from astropy.io import fits
-import aplpy
-from constants import distance
-import pylab as pl
+from astropy.table import Table
+import masscalc
 
 regions = pyregion.open(paths.rpath('cores.reg'))
 
-data = fits.open(paths.dpath('w51_spw3_continuum_r0_mulstiscale.image.fits'))
+contfile = fits.open(paths.dpath('w51_spw3_continuum_r0_mulstiscale.image.fits'))
+data = contfile[0].data
+
+results = {}
+units = {'peak':u.Jy/u.beam,
+         'sum':u.Jy/u.beam,
+         'npix':u.dimensionless_unscaled,
+         'peak_mass':u.M_sun,
+         'peak_col':u.cm**-2}
+
+for reg in regions:
+    if 'text' not in reg.attr[1]:
+        continue
+
+    shreg = pyregion.ShapeList([reg])
+    name = reg.attr[1]['text']
+    log.info(name)
+
+    mask = shreg.get_mask(hdu=contfile[0])
+
+    results[name] = {'peak': data[mask].max(),
+                     'sum': data[mask].sum(),
+                     'npix': mask.sum(),
+                    }
+    results[name]['peak_mass'] = masscalc.mass_conversion_factor()*results[name]['peak']
+    results[name]['peak_col'] = masscalc.col_conversion_factor()*results[name]['peak']
+
+# invert the table to make it parseable by astropy...
+# (this shouldn't be necessary....)
+results_inv = {}
+columns = {}
+for k,v in results.items():
+    for kk,vv in v.items():
+        if kk in results_inv:
+            results_inv[kk][k] = vv
+            columns[kk].append(vv)
+        else:
+            results_inv[kk] = {k:vv}
+            columns[kk] = [vv]
+
+for c in columns:
+    columns[c] = columns[c] * units[c]
+
+tbl = Table(columns)
+
+tbl.write(paths.tpath("continuum_photometry.ipac"), format='ascii.ipac')
--- code/masscalc.py
+++ code/masscalc.py
@@ -1,6 +1,9 @@
 import numpy as np
-def mass_conversion_factor(TK = 20, d=5.4):
-    return 14.30 * (np.exp(13.01/TK) - 1)*d**2
-def col_conversion_factor(TK = 20):
-    return 2.19e22 * (np.exp(13.01/TK - 1))
+from astropy import units as u
+from constants import distance
 
+def mass_conversion_factor(TK=20, d=distance.to(u.kpc).value):
+    return 14.30 * (np.exp(13.01/TK) - 1)*d**2
+
+def col_conversion_factor(TK=20):
+    return 2.19e22 * (np.exp(13.01/TK - 1))
