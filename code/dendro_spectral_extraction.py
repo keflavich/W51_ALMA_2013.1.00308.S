ffdb170215f291e2543c7e02d5b477fde3ae1375
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-03-11 09:34:07 +0100

    fix merge scripts: allspw supported now, and multiscale will actually be used

--- code/dendro_spectral_extraction.py
+++ code/dendro_spectral_extraction.py
@@ -2,29 +2,39 @@ import numpy as np
 from spectral_cube import SpectralCube
 from astropy.io import fits
 from astropy.table import Table
+import FITS_tools
+import pyregion
 
 tmplt = "full_W51_spw{0}_lines.fits"
 
 pruned_ppcat = Table.read("dendrogram_continuum_catalog.ipac",
                           format='ascii.ipac')
-dendromask = fits.getdata('dendrograms_min1mJy_diff1mJy_mask_pruned.fits')
+#dendromask = fits.open('dendrograms_min1mJy_diff1mJy_mask_pruned.fits')
 
 for spw in (0,1,2,3):
     cube = SpectralCube.read(tmplt.format(spw))
     print(cube)
 
+    #mask_reproj = FITS_tools.hcongrid.hastrom(dendromask[0].data.astype('float'),
+    #                                          dendromask[0].header,
+    #                                          FITS_tools.strip_headers.flatten_header(cube.header))
+    #rmask = np.round(mask_reproj.data).astype('int')
+
     for row in pruned_ppcat:
         name = row['_idx']
         print("Extracting {0} from {1}".format(name, spw))
+        SL = pyregion.parse("fk5; circle({0},{1},0.5\")"
+                            .format(row['x_cen'], row['y_cen']))
 
-        mask = dendromask == name
-        dend_inds = np.where(mask)
+        #mask = rmask == name
+        #dend_inds = np.where(mask)
 
-        view = (slice(None), # all spectral channels
-                slice(dend_inds[0].min(), dend_inds[0].max()+1),
-                slice(dend_inds[1].min(), dend_inds[1].max()+1),
-               )
-        sc = cube[view].with_mask(mask[view[1:]])
+        #view = (slice(None), # all spectral channels
+        #        slice(dend_inds[0].min(), dend_inds[0].max()+1),
+        #        slice(dend_inds[1].min(), dend_inds[1].max()+1),
+        #       )
+        #sc = cube[view].with_mask(mask[view[1:]])
+        sc = cube.subcube_from_ds9region(SL)
         spec = sc.mean(axis=(1,2))
         spec.hdu.writeto("spectra/dendro{0:03d}_spw{1}_mean.fits".format(name, spw),
                          clobber=True)
--- script_merge/selfcal_scriptForImaging_continuum_flag_spw3.py
+++ script_merge/selfcal_scriptForImaging_continuum_flag_spw3.py
@@ -9,58 +9,7 @@ fields_to_selfcal = (31,32,33,39,40,24,25,20,13,21,27)
 fields_after_split = [f-4 for f in fields_to_selfcal]
 field = ",".join([str(x) for x in fields_after_split])
 
-
-mergevis = 'continuum_7m12m_allspw.ms'
-if not os.path.exists(mergevis):
-    for spw in range(4):
-        with open('linechannels12m_spw{0}'.format(spw),'r') as f:
-            linechannels7m = linechannels12m = f.read()
-
-        finalvis12m='calibrated_12m.ms'
-        contvis12m='w51_spw{0}_continuum_flagged_12m.split'.format(spw)
-        flagmanager(vis=finalvis12m,mode='save',
-                    versionname='before_cont_flags')
-
-        flagdata(vis=finalvis12m,mode='manual',
-                 spw=linechannels12m,flagbackup=False)
-
-        split(vis=finalvis12m,
-              spw='{0},{1}'.format(spw,spw+4),
-              outputvis=contvis12m,
-              width=[192,192],
-              datacolumn='data')
-
-        flagmanager(vis=finalvis12m, mode='restore',
-                    versionname='before_cont_flags')
-
-        with open('linechannels7m_spw{0}'.format(spw),'r') as f:
-            linechannels7m = f.read()
-        finalvis7m='calibrated_7m.ms'
-        contvis7m='w51_spw{0}_continuum_flagged_7m.split'.format(spw)
-        flagmanager(vis=finalvis7m,mode='save',
-                    versionname='before_cont_flags')
-
-        flagdata(vis=finalvis7m,mode='manual',
-                 spw=linechannels7m,flagbackup=False)
-
-        split(vis=finalvis7m,
-              spw='{0}'.format(spw),
-              outputvis=contvis7m,
-              width=[192],
-              datacolumn='data')
-
-
-        flagmanager(vis=finalvis7m,mode='restore',
-                    versionname='before_cont_flags')
-
-    to_concat = (['w51_spw{0}_continuum_flagged_12m.split'.format(spw)
-                 for spw in range(4)] +
-                 ['w51_spw{0}_continuum_flagged_12m.split'.format(spw)
-                 for spw in range(4)])
-
-    concat(vis=to_concat, concatvis=mergevis)
-
-contvis='continuum_7m12m_allspw.ms'
+contvis='continuum_7m12m.ms'
 vis0 = 'w51_continuum_7m12m_contvis_selfcal_0.ms'
 
 os.system('rm -rf {0}'.format(vis0))
@@ -88,7 +37,7 @@ multiscale = [0,5,15,45,135]
 
 clearcal(vis=vis0)
 #flagmanager(vis=vis0, versionname='flagdata_1', mode='restore')
-myimagebase = "merge_selfcal_allspw_dirty"
+myimagebase = "merge_selfcal_spw3_dirty"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis0, imagename=myimagebase, field="", spw='',
        specmode='mfs', outframe='LSRK', interpolation='linear', gridder='mosaic',
@@ -98,7 +47,7 @@ tclean(vis=vis0, imagename=myimagebase, field="", spw='',
 exportfits(myimagebase+'.model', myimagebase+'.model.fits', dropdeg=True, overwrite=True)
 exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, overwrite=True)
 
-dirtyimage = 'merge_selfcal_allspw_dirty.residual'
+dirtyimage = 'merge_selfcal_spw3_dirty.residual'
 ia.open(dirtyimage)
 ia.calcmask(mask=dirtyimage+" > 0.1", name='dirty_mask_100mJy')
 ia.close()
@@ -109,7 +58,7 @@ exportfits('dirty_100mJy.mask', 'dirty_100mJy.mask.fits', dropdeg=True, overwrit
 
 
 # rms ~3.7 mJy/beam
-myimagebase = "merge_selfcal_allspw_mfs"
+myimagebase = "merge_selfcal_spw3_mfs"
 # threshold = 50mJy with no other restrictions -> infinite divergence
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis0, imagename=myimagebase, field="", spw='',
@@ -126,11 +75,11 @@ exportfits(myimagebase+'.image.pbcor', myimagebase+'.image.pbcor.fits', dropdeg=
 exportfits(myimagebase+'.model', myimagebase+'.model.fits', dropdeg=True, overwrite=True)
 exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, overwrite=True)
 
-rmtables('selfcal_allspw_phase.cal')
-gaincal(vis=vis0, caltable="selfcal_allspw_phase.cal", field=field, solint='inf',
+rmtables('selfcal_spw3_phase.cal')
+gaincal(vis=vis0, caltable="selfcal_spw3_phase.cal", field=field, solint='inf',
         calmode="p", refant="", gaintype="G", minsnr=5, uvrange='100~5000m')
 
-image1 = 'merge_selfcal_allspw_mfs.image'
+image1 = 'merge_selfcal_spw3_mfs.image'
 ia.open(image1)
 ia.calcmask(mask=image1+" > 0.05", name='clean_mask_50mJy')
 ia.close()
@@ -140,7 +89,7 @@ makemask(mode='copy', inpimage=image1,
          overwrite=True)
 
 flagmanager(vis=vis0, mode='save', versionname='backup')
-applycal(vis=vis0, field="", gaintable=["selfcal_allspw_phase.cal"],
+applycal(vis=vis0, field="", gaintable=["selfcal_spw3_phase.cal"],
          interp="linear", applymode='calonly', calwt=False)
 flagmanager(vis=vis0, mode='restore', versionname='backup')
 summary0 = flagdata(vis=vis0, mode='summary')
@@ -151,7 +100,7 @@ os.system('rm -rf {0}.flagversions'.format(vis1))
 split(vis=vis0, outputvis=vis1,
       datacolumn="corrected")
 
-myimagebase="selfcal_allspw_selfcal_mfs"
+myimagebase="selfcal_spw3_selfcal_mfs"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis1, imagename=myimagebase, field="", spw='',
        outframe='LSRK', interpolation='linear', gridder='mosaic',
@@ -168,19 +117,19 @@ exportfits(myimagebase+'.image.pbcor', myimagebase+'.image.pbcor.fits', dropdeg=
 exportfits(myimagebase+'.model', myimagebase+'.model.fits', dropdeg=True, overwrite=True)
 exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, overwrite=True)
 
-rmtables('selfcal_allspw_phase_2.cal')
-gaincal(vis=vis1, caltable="selfcal_allspw_phase_2.cal", field=field,
+rmtables('selfcal_spw3_phase_2.cal')
+gaincal(vis=vis1, caltable="selfcal_spw3_phase_2.cal", field=field,
         solint=solint, calmode="p", refant="DV07", gaintype="G", minsnr=5,
         uvrange='100~5000m')
-#plotcal(caltable="selfcal_allspw_phase_2.cal", xaxis="time", yaxis="phase", subplot=331,
+#plotcal(caltable="selfcal_spw3_phase_2.cal", xaxis="time", yaxis="phase", subplot=331,
 #        iteration="antenna", plotrange=[0,0,-30,30], markersize=5,
 #        fontsize=10.0,)
-assert len(selfcal_heuristics.goodenough_field_solutions("selfcal_allspw_phase_2.cal", minsnr=5, maxphasenoise=np.pi/4., pols=[0])) > 0
-print("Goodenough field solns: ",selfcal_heuristics.goodenough_field_solutions("selfcal_allspw_phase_2.cal", minsnr=5, maxphasenoise=np.pi/4., pols=[0]))
+assert len(selfcal_heuristics.goodenough_field_solutions("selfcal_spw3_phase_2.cal", minsnr=5, maxphasenoise=np.pi/4., pols=[0])) > 0
+print("Goodenough field solns: ",selfcal_heuristics.goodenough_field_solutions("selfcal_spw3_phase_2.cal", minsnr=5, maxphasenoise=np.pi/4., pols=[0]))
 
 
 flagmanager(vis=vis1, mode='save', versionname='backup')
-applycal(vis=vis1, field="", gaintable=["selfcal_allspw_phase_2.cal"],
+applycal(vis=vis1, field="", gaintable=["selfcal_spw3_phase_2.cal"],
          interp="linear", applymode='calonly', calwt=False)
 flagmanager(vis=vis1, mode='restore', versionname='backup')
 summary1 = flagdata(vis=vis1, mode='summary')
@@ -191,7 +140,7 @@ os.system('rm -rf {0}.flagversions'.format(vis2))
 split(vis=vis1, outputvis=vis2,
       datacolumn="corrected")
 
-myimagebase = "merge_selfcal_allspw_selfcal_2_mfs"
+myimagebase = "merge_selfcal_spw3_selfcal_2_mfs"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis2, imagename=myimagebase, field="", spw='',
        outframe='LSRK', interpolation='linear', gridder='mosaic',
@@ -207,8 +156,8 @@ exportfits(myimagebase+'.image.pbcor', myimagebase+'.image.pbcor.fits', dropdeg=
 exportfits(myimagebase+'.model', myimagebase+'.model.fits', dropdeg=True, overwrite=True)
 exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, overwrite=True)
 
-rmtables("selfcal_allspw_phase_3.cal")
-gaincal(vis=vis2, caltable="selfcal_allspw_phase_3.cal", field=field,
+rmtables("selfcal_spw3_phase_3.cal")
+gaincal(vis=vis2, caltable="selfcal_spw3_phase_3.cal", field=field,
         solint=solint, calmode="p", refant="", gaintype="G", minsnr=5,
         uvrange='100~5000m')
 #plotcal(caltable="phase_3.cal", xaxis="time", yaxis="phase", subplot=331,
@@ -216,7 +165,7 @@ gaincal(vis=vis2, caltable="selfcal_allspw_phase_3.cal", field=field,
 #        fontsize=10.0,)
 
 flagmanager(vis=vis2, mode='save', versionname='backup')
-applycal(vis=vis2, field="", gaintable=["selfcal_allspw_phase_3.cal"],
+applycal(vis=vis2, field="", gaintable=["selfcal_spw3_phase_3.cal"],
          interp="linear", applymode='calonly', calwt=False)
 flagmanager(vis=vis2, mode='restore', versionname='backup')
 summary2 = flagdata(vis=vis2, mode='summary')
@@ -227,7 +176,7 @@ os.system('rm -rf {0}.flagversions'.format(vis3))
 split(vis=vis2, outputvis=vis3,
       datacolumn="corrected")
 
-myimagebase = "merge_selfcal_allspw_selfcal_3_mfs"
+myimagebase = "merge_selfcal_spw3_selfcal_3_mfs"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis3, imagename=myimagebase, field="", spw='',
        outframe='LSRK', interpolation='linear', gridder='mosaic',
@@ -243,18 +192,18 @@ exportfits(myimagebase+'.image.pbcor', myimagebase+'.image.pbcor.fits', dropdeg=
 exportfits(myimagebase+'.model', myimagebase+'.model.fits', dropdeg=True, overwrite=True)
 exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, overwrite=True)
 
-rmtables("selfcal_allspw_phase_4.cal")
-gaincal(vis=vis3, caltable="selfcal_allspw_phase_4.cal", field=field,
+rmtables("selfcal_spw3_phase_4.cal")
+gaincal(vis=vis3, caltable="selfcal_spw3_phase_4.cal", field=field,
         solint=solint, calmode="p", refant="", gaintype="G", minsnr=5,
         uvrange='100~5000m')
 
-rmtables("selfcal_allspw_ampphase.cal")
-gaincal(vis=vis3, caltable="selfcal_allspw_ampphase.cal", field=field,
+rmtables("selfcal_spw3_ampphase.cal")
+gaincal(vis=vis3, caltable="selfcal_spw3_ampphase.cal", field=field,
         solint=solint, solnorm=True, calmode="ap", refant="", gaintype="G",
         minsnr=5, uvrange='100~5000m')
 
 flagmanager(vis=vis3, mode='save', versionname='backup')
-applycal(vis=vis3, field="", gaintable=["selfcal_allspw_phase_4.cal", 'selfcal_allspw_ampphase.cal'],
+applycal(vis=vis3, field="", gaintable=["selfcal_spw3_phase_4.cal", 'selfcal_spw3_ampphase.cal'],
          interp="linear", applymode='calonly', calwt=False)
 summary3 = flagdata(vis=vis3, mode='summary')
 print("{flagged}/{total} flagged points in vis3 afer applycal".format(**summary3))
@@ -269,7 +218,7 @@ split(vis=vis3, outputvis=vis4,
 summary4 = flagdata(vis=vis4, mode='summary')
 print("{flagged}/{total} flagged points in vis4 after restoration".format(**summary4))
 
-myimagebase = "merge_selfcal_allspw_selfcal_4ampphase_mfs"
+myimagebase = "merge_selfcal_spw3_selfcal_4ampphase_mfs"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis4, imagename=myimagebase, field="", spw='',
        outframe='LSRK', interpolation='linear', gridder='mosaic',
@@ -286,7 +235,7 @@ exportfits(myimagebase+'.model', myimagebase+'.model.fits', dropdeg=True, overwr
 exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, overwrite=True)
 
 
-myimagebase = "merge_selfcal_allspw_selfcal_4ampphase_mfs_tclean"
+myimagebase = "merge_selfcal_spw3_selfcal_4ampphase_mfs_tclean"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis4, imagename=myimagebase, field="", spw="", specmode='mfs',
        deconvolver='multiscale', gridder='mosaic', outframe='LSRK',
@@ -302,7 +251,7 @@ exportfits(myimagebase+'.image.pbcor', myimagebase+'.image.pbcor.fits', dropdeg=
 exportfits(myimagebase+'.model', myimagebase+'.model.fits', dropdeg=True, overwrite=True)
 exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, overwrite=True)
 
-myimagebase = "merge_selfcal_allspw_selfcal_4ampphase_mfs_tclean_deeper"
+myimagebase = "merge_selfcal_spw3_selfcal_4ampphase_mfs_tclean_deeper"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis4, imagename=myimagebase, field="", spw="", specmode='mfs',
        deconvolver='multiscale', gridder='mosaic', outframe='LSRK',
@@ -323,7 +272,7 @@ exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, 
 
 
 # using vis2.corrected = vis3.data
-myimagebase = "merge_selfcal_allspw_selfcal_3_mfs_deeper"
+myimagebase = "merge_selfcal_spw3_selfcal_3_mfs_deeper"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis2, imagename=myimagebase, field="", spw="", specmode='mfs',
        deconvolver='multiscale', gridder='mosaic', outframe='LSRK',
@@ -341,7 +290,7 @@ exportfits(myimagebase+'.model', myimagebase+'.model.fits', dropdeg=True, overwr
 exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, overwrite=True)
 
 
-myimagebase = "merge_selfcal_allspw_selfcal_4ampphase_mfs_tclean_deeperbroader"
+myimagebase = "merge_selfcal_spw3_selfcal_4ampphase_mfs_tclean_deeperbroader"
 os.system('rm -rf {0}.*'.format(myimagebase))
 tclean(vis=vis4, imagename=myimagebase, field="", spw="", specmode='mfs',
        deconvolver='multiscale', gridder='mosaic', outframe='LSRK',
@@ -367,20 +316,21 @@ exportfits(myimagebase+'.residual', myimagebase+'.residual.fits', dropdeg=True, 
 
 
 
+
 import numpy as np
 from astropy.io import fits
 print("Stats (mfs):")
 slc = slice(1007,1434), slice(1644,1900)
-sigma, peak = (fits.getdata('merge_selfcal_allspw_dirty.residual.fits')[slc].std(),     np.nanmax(fits.getdata('merge_selfcal_allspw_dirty.residual.fits')))
+sigma, peak = (fits.getdata('merge_selfcal_spw3_dirty.residual.fits')[slc].std(),     np.nanmax(fits.getdata('merge_selfcal_spw3_dirty.residual.fits')))
 print("dirty:             peak={1:0.5f} sigma={0:0.5f} s/n={2:0.5f}".format(sigma, peak, peak/sigma))
-sigma, peak = (fits.getdata('merge_selfcal_allspw_mfs.image.pbcor.fits')[slc].std(),           np.nanmax(fits.getdata('merge_selfcal_allspw_mfs.image.pbcor.fits')))
+sigma, peak = (fits.getdata('merge_selfcal_spw3_mfs.image.pbcor.fits')[slc].std(),           np.nanmax(fits.getdata('merge_selfcal_spw3_mfs.image.pbcor.fits')))
 print("clean:             peak={1:0.5f} sigma={0:0.5f} s/n={2:0.5f}".format(sigma, peak, peak/sigma))
-sigma, peak = (fits.getdata('merge_selfcal_allspw_mfs.image.pbcor.fits')[slc].std(),   np.nanmax(fits.getdata('merge_selfcal_allspw_mfs.image.pbcor.fits')))
+sigma, peak = (fits.getdata('merge_selfcal_spw3_mfs.image.pbcor.fits')[slc].std(),   np.nanmax(fits.getdata('merge_selfcal_spw3_mfs.image.pbcor.fits')))
 print("selfcal:           peak={1:0.5f} sigma={0:0.5f} s/n={2:0.5f}".format(sigma, peak, peak/sigma))
-sigma, peak = (fits.getdata('merge_selfcal_allspw_selfcal_2_mfs.image.pbcor.fits')[slc].std(), np.nanmax(fits.getdata('merge_selfcal_allspw_selfcal_2_mfs.image.pbcor.fits')))
+sigma, peak = (fits.getdata('merge_selfcal_spw3_selfcal_2_mfs.image.pbcor.fits')[slc].std(), np.nanmax(fits.getdata('merge_selfcal_spw3_selfcal_2_mfs.image.pbcor.fits')))
 print("selfcal2:          peak={1:0.5f} sigma={0:0.5f} s/n={2:0.5f}".format(sigma, peak, peak/sigma))
-sigma, peak = (fits.getdata('merge_selfcal_allspw_selfcal_3_mfs.image.pbcor.fits')[slc].std(), np.nanmax(fits.getdata('merge_selfcal_allspw_selfcal_3_mfs.image.pbcor.fits')))
+sigma, peak = (fits.getdata('merge_selfcal_spw3_selfcal_3_mfs.image.pbcor.fits')[slc].std(), np.nanmax(fits.getdata('merge_selfcal_spw3_selfcal_3_mfs.image.pbcor.fits')))
 print("selfcal3:          peak={1:0.5f} sigma={0:0.5f} s/n={2:0.5f}".format(sigma, peak, peak/sigma))
-sigma, peak = (fits.getdata('merge_selfcal_allspw_selfcal_4ampphase_mfs.image.pbcor.fits')[slc].std(), np.nanmax(fits.getdata('merge_selfcal_allspw_selfcal_4ampphase_mfs.image.pbcor.fits')))
+sigma, peak = (fits.getdata('merge_selfcal_spw3_selfcal_4ampphase_mfs.image.pbcor.fits')[slc].std(), np.nanmax(fits.getdata('merge_selfcal_spw3_selfcal_4ampphase_mfs.image.pbcor.fits')))
 print("selfcal4 ampphase: peak={1:0.5f} sigma={0:0.5f} s/n={2:0.5f}".format(sigma, peak, peak/sigma))
 print("Completed in {0}s".format(time.time()-t0))
