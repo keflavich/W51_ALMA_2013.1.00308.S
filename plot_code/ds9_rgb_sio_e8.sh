506dafb453360b701febb4d265fdcfc9cd8ed6f9
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-09-08 11:59:34 -0600

    added ds9 plot hacks

---
+++ plot_code/ds9_rgb_sio_e8.sh
@@ -0,0 +1,8 @@
+xpaset -p ds9 rgb
+xpaset -p ds9 rgb open
+xpaset -p ds9 rgb red
+xpaset -p ds9 fits ~/work/w51/alma/FITS/longbaseline/SiO_74to118kms_e8.fits
+xpaset -p ds9 rgb blue
+xpaset -p ds9 fits ~/work/w51/alma/FITS/longbaseline/SiO_m32to55kms_e8.fits
+xpaset -p ds9 rgb green
+xpaset -p ds9 fits ~/work/w51/alma/FITS/longbaseline/W51e2cax.cont.image.pbcor.fits
---
+++ plot_code/ds9_rgb_sio_north.sh
@@ -0,0 +1,8 @@
+xpaset -p ds9 rgb
+xpaset -p ds9 rgb open
+xpaset -p ds9 rgb red
+xpaset -p ds9 fits ~/work/w51/alma/FITS/longbaseline/SiO_74to118kms_north.fits
+xpaset -p ds9 rgb blue
+xpaset -p ds9 fits ~/work/w51/alma/FITS/longbaseline/SiO_m32to55kms_north.fits
+xpaset -p ds9 rgb green
+xpaset -p ds9 fits ~/work/w51/alma/FITS/longbaseline/W51ncax.cont.image.pbcor.fits
