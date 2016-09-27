997375abe774854b0b9226c0ccbb2b4e8f53ff5a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-09-27 12:10:19 -0600

    radial jeans analysis

--- analysis/radial_profiles.py
+++ analysis/radial_profiles.py
@@ -2,8 +2,10 @@ import numpy as np
 import radio_beam
 import pyregion
 import paths
-import image_tools
+#import image_tools
+from image_tools.radialprofile import azimuthalAverage
 from astropy import wcs
+from astropy import constants
 from astropy.nddata import Cutout2D
 from astropy.io import fits
 from astropy import units as u
@@ -11,6 +13,8 @@ from astropy import coordinates
 import pylab as pl
 import itertools
 import masscalc
+import dust_emissivity
+import reproject
 
 ffiles = """
 selfcal_allspw_mfs.image.pbcor.fits
@@ -68,14 +72,13 @@ def make_rprof(regions, ploteach=False):
 
             pixscale = (mywcs.pixel_scale_matrix.diagonal()**2).sum()**0.5
             ppbeam = (beam.sr/(pixscale**2*u.deg**2)).decompose().value / u.beam
-            print("fn  {0} ppbeam={1:0.2f}".format(fn, ppbeam))
+            #print("fn  {0} ppbeam={1:0.2f}".format(fn, ppbeam))
             
             for ii,(name,position) in enumerate(zip(names, center_positions)):
                 cutout = Cutout2D(fh[0].data, position, size, wcs=mywcs)
 
-                nr, bins, rprof = image_tools.radialprofile.azimuthalAverage(cutout.data,
-                                                                             binsize=1.0,
-                                                                             return_nr=True)
+                nr, bins, rprof = azimuthalAverage(cutout.data, binsize=1.0,
+                                                   return_nr=True)
 
                 linestyle = next(linestyles[name])
 
@@ -135,11 +138,8 @@ def make_rprof(regions, ploteach=False):
         nplots = 0
 
     pl.matplotlib.rc_file('pubfiguresrc')
-    pl.figure(nplots*3+1).clf()
-    pl.figure(nplots*3+2).clf()
-    pl.figure(nplots*3+3).clf()
-    pl.figure(nplots*3+4).clf()
-    pl.figure(nplots*3+5).clf()
+    for jj in range(nplots*3+1, nplots*3+9):
+        pl.figure(jj).clf()
     # $ find ~/work/w51/alma/FITS/ -samefile ~/work/w51/alma/FITS/W51_te_continuum_best.fits
     # /Users/adam/work/w51/alma/FITS//12m/continuum/selfcal_allspw_selfcal_3_mfs_deeper.image.pbcor.fits
     # /Users/adam/work/w51/alma/FITS//W51_te_continuum_best.fits
@@ -152,9 +152,8 @@ def make_rprof(regions, ploteach=False):
     for ii,(name,position) in enumerate(zip(names, center_positions)):
         cutout = Cutout2D(fh[0].data, position, size, wcs=mywcs)
 
-        nr, bins, rprof = image_tools.radialprofile.azimuthalAverage(cutout.data,
-                                                                     binsize=1.0,
-                                                                     return_nr=True)
+        nr, bins, rprof = azimuthalAverage(cutout.data, binsize=1.0,
+                                           return_nr=True)
 
         pl.figure(nplots*3+1)
         #pl.title(fn.replace(".image.pbcor.fits",""))
@@ -233,8 +232,8 @@ def make_rprof(regions, ploteach=False):
 
 
         density_40k_profile = (mass_40k_profile / (4/3.*np.pi*radii**3) / (2.8*u.Da)).to(u.cm**-3)
-        print(density_40k_profile)
-        print(radii)
+        #print(density_40k_profile)
+        #print(radii)
 
         pl.figure(nplots*3+4)
         #pl.title(fn.replace(".image.pbcor.fits",""))
@@ -286,8 +285,9 @@ def make_rprof(regions, ploteach=False):
 
         if ii == len(names) - 1:
             ax = pl.gca()
-            #ax2 = ax.twiny()
-            ax3 = ax.twinx()
+            ax.set_ylim(1e7, 5e10)
+            ax2 = ax.twiny()
+            #ax3 = ax.twinx()
             def tick_function(old_x):
                 newx = (old_x*u.arcsec*masscalc.distance).to(u.au, u.dimensionless_angles()).value
                 return ["%.1f" % z for z in newx]
@@ -305,6 +305,172 @@ def make_rprof(regions, ploteach=False):
             #ax3.set_ylabel("Cumulative Mass (M$_\\odot$, $T=40$ K)")
 
 
+        c_s_40k = ((constants.k_B * 40*u.K / (2.4*u.Da))**0.5).to(u.km/u.s)
+        azimuthal_average_MJ_40k = (np.pi/6. * c_s_40k**3 /
+                                    (constants.G**1.5 *
+                                     (2.8*u.Da*azimuthal_average_density)**0.5)).to(u.M_sun)
+
+        pl.figure(nplots*3+6)
+        pl.semilogy(angular_radii, azimuthal_average_MJ_40k, label=name)
+        pl.ylabel("Azimuthally Averaged $M_J$ at $T=40$K")
+        pl.xlabel("Radius (arcsec)")
+        if len(names) < 5:
+            pl.legend(loc='best')
+        elif ii==len(names)-1:
+            ax = pl.gca()
+            box = ax.get_position()
+            ax.set_position([box.x0, box.y0, box.width * 0.6, box.height])
+
+            # Put a legend to the right of the current axis
+            ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+        if ii == len(names) - 1:
+            ax = pl.gca()
+            #ax.set_ylim(1e7, 5e10)
+            ax2 = ax.twiny()
+            #ax3 = ax.twinx()
+            def tick_function(old_x):
+                newx = (old_x*u.arcsec*masscalc.distance).to(u.au, u.dimensionless_angles()).value
+                return ["%.1f" % z for z in newx]
+            new_tick_locations = np.arange(0,8000,1000)*u.au
+            new_tick_locs_as = (new_tick_locations/masscalc.distance).to(u.arcsec, u.dimensionless_angles())
+            ax2.set_xlim(ax.get_xlim())
+            ax2.set_xticks(new_tick_locs_as.value)
+            ax2.set_xticklabels(tick_function(new_tick_locs_as.value))
+            ax2.set_xlabel(r"Radius (au)")
+
+        source = 'e2' if name == 'e2e' else name
+        temperature_map_fn = paths.dpath('12m/moments/CH3OH_{0}_cutout_temperaturemap.fits'.format(source))
+        temperature_map_fh = fits.open(temperature_map_fn)
+
+        # this whole section is copied from overlay_contours_on_ch3oh
+        ch3ohN_hdul = fits.open(paths.dpath('12m/moments/CH3OH_{0}_cutout_columnmap.fits'.format(source)))
+        ch3ohT_hdul = fits.open(paths.dpath('12m/moments/CH3OH_{0}_cutout_temperaturemap.fits'.format(source)))
+        bigwcs = wcs.WCS(ch3ohT_hdul[0].header)
+        bigpixscale = (bigwcs.pixel_scale_matrix.diagonal()**2).sum()**0.5 * u.deg
+        ch3ohN = ch3ohN_hdul[0].data
+        ch3ohT = ch3ohT_hdul[0].data
+        dust_brightness,wts = reproject.reproject_interp(fits.open(paths.dpath('W51_te_continuum_best.fits')),
+                                                         ch3ohN_hdul[0].header)
+        bm = radio_beam.Beam.from_fits_header(paths.dpath("W51_te_continuum_best.fits"))
+
+        yy,xx = np.indices(ch3ohN.shape)
+        if source == 'north':
+            center = [84.,38.]
+        else:
+            center = [ch3ohN.shape[0]/2., ch3ohN.shape[1]/2.]
+        yyc = (yy-center[0])
+        xxc = (xx-center[1])
+        rr = (yyc**2 + xxc**2)**0.5
+        rr_as = (rr*bigpixscale).to(u.arcsec)
+        theta = np.arctan2(yyc,xxc)*u.rad
+
+        dust_column = dust_emissivity.dust.colofsnu(225*u.GHz, dust_brightness*u.Jy,
+                                                    beamomega=bm,
+                                                    temperature=ch3ohT*u.K)
+        ch3oh_abundance = ch3ohN / dust_column.value
+        mask = (ch3oh_abundance > 1e-10) & (ch3oh_abundance < 1e-5)
+        if source == 'e2':
+            mask = mask & (((theta > 15*u.deg) & (theta < 345*u.deg)) | (theta < -15*u.deg))
+        mask = mask & np.isfinite(ch3oh_abundance)
+        # exclude high-abundance, low-column regions: likely to be div-by-zero zones
+        mask = mask & (~((ch3ohN < 1e18) & (ch3oh_abundance > 5e-6)))
+        mask = mask & (~((dust_brightness<1e-2) & (ch3ohT > 500) & (ch3oh_abundance > 1e-6)))
+
+        #mask = mask & (~((ch3ohT > 250) &
+        #                 (ch3ohN < 1e18) &
+        #                 (rr_as>1.5*u.arcsec))
+        #               )# these are low-column,
+
+        temwcs = wcs.WCS(temperature_map_fh[0].header)
+
+        temcutout = Cutout2D(temperature_map_fh[0].data, position, size, wcs=temwcs)
+        maskcutout = Cutout2D(mask.astype('float'), position, size, wcs=bigwcs)
+
+        tem_nr, tem_bins, tem_rprof = azimuthalAverage(temcutout.data,
+                                                       weights=(maskcutout.data>0).astype('float'),
+                                                       binsize=1.0,
+                                                       return_nr=True,
+                                                       interpnan=True,
+                                                      )
+
+        mass_profile = (cumul_rprof * masscalc.mass_conversion_factor(TK=tem_rprof) / u.beam).to(u.M_sun)
+        density_profile = (mass_profile / (4/3.*np.pi*radii**3) / (2.8*u.Da)).to(u.cm**-3)
+
+        c_s = ((constants.k_B * tem_rprof*u.K / (2.4*u.Da))**0.5).to(u.km/u.s)
+        azimuthal_average_MJ = (np.pi/6. * c_s**3 /
+                                (constants.G**1.5 *
+                                 (2.8*u.Da*azimuthal_average_density)**0.5)
+                               ).to(u.M_sun)
+
+
+        pl.figure(nplots*3+7)
+        pl.plot(angular_radii, azimuthal_average_MJ, label=name)
+        pl.ylabel("Azimuthally Averaged $M_J$ at $T(\\mathrm{CH}_3\\mathrm{OH})$")
+        pl.xlabel("Radius (arcsec)")
+        if len(names) < 5:
+            pl.legend(loc='best')
+        elif ii==len(names)-1:
+            ax = pl.gca()
+            box = ax.get_position()
+            ax.set_position([box.x0, box.y0, box.width * 0.6, box.height])
+
+            # Put a legend to the right of the current axis
+            ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+        if ii == len(names) - 1:
+            ax = pl.gca()
+            #ax.set_ylim(1e7, 5e10)
+            ax2 = ax.twiny()
+            #ax3 = ax.twinx()
+            def tick_function(old_x):
+                newx = (old_x*u.arcsec*masscalc.distance).to(u.au, u.dimensionless_angles()).value
+                return ["%.1f" % z for z in newx]
+            new_tick_locations = np.arange(0,8000,1000)*u.au
+            new_tick_locs_as = (new_tick_locations/masscalc.distance).to(u.arcsec, u.dimensionless_angles())
+            ax2.set_xlim(ax.get_xlim())
+            ax2.set_xticks(new_tick_locs_as.value)
+            ax2.set_xticklabels(tick_function(new_tick_locs_as.value))
+            ax2.set_xlabel(r"Radius (au)")
+
+
+        pl.figure(nplots*3+8)
+        #pl.title(fn.replace(".image.pbcor.fits",""))
+        pl.plot(bins*pixscale*3600., mass_profile,
+                label=name)
+        pl.ylabel("Cumulative Mass at T(CH$_3$OH)")
+        pl.xlabel("Radius (arcsec)")
+        if len(names) < 5:
+            pl.legend(loc='best')
+        elif ii==len(names)-1:
+            ax = pl.gca()
+            box = ax.get_position()
+            ax.set_position([box.x0, box.y0, box.width * 0.6, box.height])
+
+            # Put a legend to the right of the current axis
+            ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+        if ii == len(names) - 1:
+            ax = pl.gca()
+            ax2 = ax.twiny()
+            def tick_function(old_x):
+                newx = (old_x*u.arcsec*masscalc.distance).to(u.au, u.dimensionless_angles()).value
+                return ["%.1f" % z for z in newx]
+            new_tick_locations = np.arange(0,8000,1000)*u.au
+            new_tick_locs_as = (new_tick_locations/masscalc.distance).to(u.arcsec, u.dimensionless_angles())
+            ax2.set_xlim(ax.get_xlim())
+            ax2.set_xticks(new_tick_locs_as.value)
+            ax2.set_xticklabels(tick_function(new_tick_locs_as.value))
+            ax2.set_xlabel(r"Radius (au)")
+
+        pl.figure(nplots*3+9)
+        #pl.title(fn.replace(".image.pbcor.fits",""))
+        pl.plot(bins*pixscale*3600., tem_rprof,
+                label=name)
+        pl.legend(loc='best')
+        pl.ylabel("CH$_3$OH temperature")
+        pl.xlabel("Radius (as)")
+
 
 
 regions = pyregion.open(paths.rpath("hmcore_centroids.reg"))
@@ -312,6 +478,11 @@ make_rprof(regions, ploteach=True)
 nplots = len(regions)
 pl.figure(nplots*3+2).savefig(paths.fpath("cumulative_radial_flux_massivecores.png"))
 pl.figure(nplots*3+3).savefig(paths.fpath("cumulative_radial_mass_massivecores.png"))
+pl.figure(nplots*3+4).savefig(paths.fpath("cumulative_density_40K_massivecores.png"))
+pl.figure(nplots*3+5).savefig(paths.fpath("azimuthalaverage_density_40K_massivecores.png"))
+pl.figure(nplots*3+6).savefig(paths.fpath("azimuthalaverage_radial_mass_40K_massivecores.png"))
+pl.figure(nplots*3+7).savefig(paths.fpath("azimuthalaverage_radial_mass_of_TCH3OH_massivecores.png"))
+pl.figure(nplots*3+8).savefig(paths.fpath("cumulative_radial_mass_of_TCH3OH_massivecores.png"))
 
 #regions = pyregion.open(paths.rpath("cores.reg"))
 #make_rprof(regions, ploteach=False)
