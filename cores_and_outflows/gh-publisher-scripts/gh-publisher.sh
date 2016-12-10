55585fd6a82082acfc67f3387c9a1dbed006809e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-12-09 21:54:27 -0700

    BUILD DAMN YOU!!

--- cores_and_outflows/gh-publisher-scripts/gh-publisher.sh
+++ cores_and_outflows/gh-publisher-scripts/gh-publisher.sh
@@ -54,6 +54,14 @@ tmpdir=$(mktemp -t publish.XXXXXX -d)
 )
 publish_dir="$tmpdir/publish"
 
+
+# get revtex
+wget https://journals.aps.org/files/revtex/4/revtex4.zip
+unzip revtex4.zip -d $tmpdir
+unzip revtex4.zip -d $project_dir
+unzip revtex4.zip -d $scripts_dir
+
+
 export GH_PUBLISHER_PROJECT_DIR="$project_dir"
 export GH_PUBLISHER_PUBLISH_DIR="$publish_dir"
 export GH_PUBLISHER_SCRIPTS_DIR="$scripts_dir"
