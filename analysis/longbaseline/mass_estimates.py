20ce34c3f656af6b5872046d2ce194e3acef7efe
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-10-20 14:50:35 +0200

    longbaseline work

--- analysis/longbaseline/mass_estimates.py
+++ analysis/longbaseline/mass_estimates.py
@@ -33,39 +33,45 @@ beam_lores = radio_beam.Beam.from_fits_header(lores_contfile[0].header)
 #contfile = fits.open(paths.dpath('selfcal_spw3_selfcal_4ampphase_mfs_tclean_deeper_10mJy.image.pbcor.fits'))
 #ln selfcal_allspw_selfcal_4ampphase_mfs_tclean_deeper_5mJy.image.pbcor.fits W51_te_continuum_best.fits
 #ln selfcal_allspw_selfcal_4ampphase_mfs_tclean_deeper_5mJy.residual.fits W51_te_continuum_best_residual.fits
-contfile_e2e8 = fits.open(paths.dpath('longbaseline/W51e2cax.cont.image.pbcor.fits'))
+#contfile_e2e8 = fits.open(paths.dpath('longbaseline/W51e2cax.cont.image.pbcor.fits'))
+contfile_e2e8 = fits.open(paths.dpath('longbaseline/W51e2_cont_briggsSC_tclean.image.fits'))
 datae2e8 = u.Quantity(contfile_e2e8[0].data.squeeze(), unit=contfile_e2e8[0].header['BUNIT'])
 
 beam_e2e8 = radio_beam.Beam.from_fits_header(contfile_e2e8[0].header)
+
+wcs_e2e8 = wcs.WCS(contfile_e2e8[0].header)
 
 # this section repeats the analysis of dust_proerties, but it serves as a nice
 # independent check since I did it 6 months later.... give or take...
 beam_radius_e2e8 = ((beam_e2e8.sr/(2*np.pi))**0.5 * masscalc.distance).to(u.pc,
                                                                           u.dimensionless_angles())
 
-contfile_north = fits.open(paths.dpath('longbaseline/W51ncax.cont.image.pbcor.fits'))
+#contfile_north = fits.open(paths.dpath('longbaseline/W51ncax.cont.image.pbcor.fits'))
+contfile_north = fits.open(paths.dpath('longbaseline/W51n_cont_briggsSC_tclean.image.fits'))
 datanorth = u.Quantity(contfile_north[0].data.squeeze(), unit=contfile_north[0].header['BUNIT'])
 
 beam_north = radio_beam.Beam.from_fits_header(contfile_north[0].header)
 
+wcs_north = wcs.WCS(contfile_north[0].header)
+
 # this section repeats the analysis of dust_proerties, but it serves as a nice
 # independent check since I did it 6 months later.... give or take...
-beam_radius_north = ((beam_north.sr/(2*np.pi))**0.5 * masscalc.distance).to(u.pc,
-                                                                          u.dimensionless_angles())
+beam_radius_north = ((beam_north.sr/(2*np.pi))**0.5 *
+                     masscalc.distance).to(u.pc, u.dimensionless_angles())
 
 e2 = regions.CircleSkyRegion(coordinates.SkyCoord('19:23:43.969 +14:30:34.518', frame='fk5', unit=(u.hour, u.deg)),
                              radius=0.616*u.arcsec)
-e2pix = e2.to_pixel(wcs.WCS(contfile_e2e8[0].header))
+e2pix = e2.to_pixel(wcs_e2e8)
 e2mask = e2pix.to_mask()
 
 e8 = regions.CircleSkyRegion(coordinates.SkyCoord('19:23:43.907 +14:30:28.267', frame='fk5', unit=(u.hour, u.deg)),
                              radius=0.464*u.arcsec)
-e8pix = e8.to_pixel(wcs.WCS(contfile_e2e8[0].header))
+e8pix = e8.to_pixel(wcs_e2e8)
 e8mask = e8pix.to_mask()
 
 north = regions.CircleSkyRegion(coordinates.SkyCoord('19:23:40.054 +14:31:05.513', frame='fk5', unit=(u.hour, u.deg)),
                                 radius=0.412*u.arcsec)
-northpix = north.to_pixel(wcs.WCS(contfile_north[0].header))
+northpix = north.to_pixel(wcs_north)
 northmask = northpix.to_mask()
 
 def masked_where(condition, data):
@@ -97,7 +103,7 @@ e2etbpeak = e2epeak * beam_e2e8.jtok(masscalc.centerfreq) * u.beam/u.Jy
 print("e2e peak brightness tem: {0}".format(e2etbpeak))
 e2e_blackbody_luminosity = (constants.sigma_sb * e2etbpeak**4 *
                             (4*np.pi*beam_radius_e2e8**2)).to(u.L_sun)
-print("e2e blackbody luminosity: {0} = {0:e}".format(e2e_blackbody_luminosity,))
+print("e2e blackbody luminosity: {0} = {0:e} = 10^{1}".format(e2e_blackbody_luminosity, np.log10(e2e_blackbody_luminosity.value)))
 
 e8peak = np.nanmax(cutout_e8)
 print("e8 peak flux: {0}".format(e8peak))
@@ -105,7 +111,7 @@ e8tbpeak = e8peak * beam_e2e8.jtok(masscalc.centerfreq) * u.beam/u.Jy
 print("e8 peak brightness tem: {0}".format(e8tbpeak))
 e8_blackbody_luminosity = (constants.sigma_sb * e8tbpeak**4 *
                            (4*np.pi*beam_radius_e2e8**2)).to(u.L_sun)
-print("e8 blackbody luminosity: {0} = {0:e}".format(e8_blackbody_luminosity,))
+print("e8 blackbody luminosity: {0} = {0:e} = 10^{1}".format(e8_blackbody_luminosity, np.log10(e8_blackbody_luminosity.value)))
 
 
 northpeak = np.nanmax(cutout_north)
@@ -114,7 +120,7 @@ northtbpeak = northpeak * beam_north.jtok(masscalc.centerfreq) * u.beam/u.Jy
 print("north peak brightness tem: {0}".format(northtbpeak))
 north_blackbody_luminosity = (constants.sigma_sb * northtbpeak**4 *
                               (4*np.pi*beam_radius_north**2)).to(u.L_sun)
-print("north blackbody luminosity: {0} = {0:e}".format(north_blackbody_luminosity,))
+print("north blackbody luminosity: {0} = {0:e} = 10^{1}".format(north_blackbody_luminosity, np.log10(north_blackbody_luminosity.value)))
 print()
 
 
@@ -134,9 +140,9 @@ integ_e2_lores = (cutout_e2_lores[cutout_e2_lores > 3*u.mJy/u.beam]).sum() * u.b
 integ_e8_lores = (cutout_e8_lores[cutout_e8_lores > 3*u.mJy/u.beam]).sum() * u.beam / ppbeam_lores
 integ_north_lores = (cutout_north_lores[cutout_north_lores > 3*u.mJy/u.beam]).sum() * u.beam / ppbeam_lores
 
-print("Integrated intensity of    e2: {0:0.2f}  /  {1:0.2f}".format(integ_e2, integ_e2_lores))
-print("Integrated intensity of    e8: {0:0.2f}  /  {1:0.2f}".format(integ_e8, integ_e8_lores))
-print("Integrated intensity of north: {0:0.2f}  /  {1:0.2f}".format(integ_north, integ_north_lores))
+print("Integrated intensity of    e2: {0:0.2f}  /  {1:0.2f} -> {2:0.1f}% recovered".format(integ_e2, integ_e2_lores, integ_e2/integ_e2_lores*100))
+print("Integrated intensity of    e8: {0:0.2f}  /  {1:0.2f} -> {2:0.1f}% recovered".format(integ_e8, integ_e8_lores, integ_e8/integ_e8_lores*100))
+print("Integrated intensity of north: {0:0.2f}  /  {1:0.2f} -> {2:0.1f}% recovered".format(integ_north, integ_north_lores, integ_north/integ_north_lores*100))
 print()
 
 integmass_e2 = integ_e2 * masscalc.mass_conversion_factor(e2etbpeak)/u.Jy
@@ -156,3 +162,10 @@ integmass_north = integ_north * masscalc.mass_conversion_factor(100*u.K)/u.Jy
 print("Integrated mass of e2 at T=100K: {0}".format(integmass_e2))
 print("Integrated mass of e8 at T=100K: {0}".format(integmass_e8))
 print("Integrated mass of north at T=100K: {0}".format(integmass_north))
+
+
+# use dendrograms to identify structures and measure masses
+dende2 = astrodendro.Dendrogram.compute(cutout_e2.value, min_value=0.001,
+                                        min_delta=0.0005, wcs=wcse2e8)
+$
+
